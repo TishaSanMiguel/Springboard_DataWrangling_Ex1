e3ca615c6fadfbb67364285e7829480ca53a7966
San Miguel <tishas@amgen.com>
2018-03-01 09:06:39 -0800

    line 24-27 from " to ` line 46-49 product_code to `Product category`

--- Springboard_DataWrangling_Ex1/DS_DW_ex1.R
+++ Springboard_DataWrangling_Ex1/DS_DW_ex1.R
@@ -21,16 +21,16 @@ View(raw_data)
 #create a product category
 raw_data <-raw_data %>%
   mutate("Product category" = case_when(
-    "product code" == "p" ~ "Smartphone",
-    "product code" == "v" ~ "TV",
-    "product code" == "x" ~ "Laptop",
-    "product code" == "q" ~ "Tablet"
+    `product code`== "p" ~ "Smartphone",
+    `product code` == "v" ~ "TV",
+    `product code` == "x" ~ "Laptop",
+    `product code` == "q" ~ "Tablet"
   ))
 View(raw_data)
 
 #add full ddress for geocoding
 raw_data <-raw_data %>%
-unite(raw_data, "full_address", address, city, country, sep = ",")
+unite("full_address", address, city, country, sep = ",")
 View(raw_data)
 
 #creat dummy variables for company
@@ -43,10 +43,10 @@ View(raw_data)
 
 #creat dummy variables for product code
 raw_data <-raw_data %>%
-  mutate(product_smartphone = ifelse(product_code == "Smartphone", 1, 0),
-         product_tv = ifelse(product_code == "TV", 1, 0),
-         product_laptop = ifelse(product_code == "Laptop", 1, 0),
-         product_tablet = ifelse(product_code == "Tablet", 1, 0))
+  mutate(product_smartphone = ifelse(`Product category` == "Smartphone", 1, 0),
+         product_tv = ifelse(`Product category` == "TV", 1, 0),
+         product_laptop = ifelse(`Product category` == "Laptop", 1, 0),
+         product_tablet = ifelse(`Product category` == "Tablet", 1, 0))
 View(raw_data)
   
   
